bde4525851a2ae32b36bbe326754fd829c0f070c
Yoshik18 <yoshikkarnawat@gmail.com>
2022-01-13 15:48:55 +0530

    Done creating models and model forms, starting out on static files

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.projects, name="projects"),
-    path('project/<str:pk>', views.project, name="project")
+    path('', views.projects, name="projects"),
+    path('project/<str:pk>/', views.project, name="project"),
+    path('createProject', views.createProject, name="createProject")
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Project
+from .forms import ProjectForm
 # Create your views here.
 
 
 def projects(request):
-    return render(request, 'projects.html')
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'single-projects.html')
+    projectObj = Project.objects.get(id=pk)
+    context = {'projectObj': projectObj}
+    return render(request, 'projects/single-project.html', context)
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
