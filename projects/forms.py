7958607256891dd8cb8bcf9dc3913176375a3ff4
Yoshik18 <yoshikkarnawat@gmail.com>
2022-01-14 16:55:21 +0530

    Added theme for main page, projects and create project form

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -7,3 +8,14 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'description', 'featured_image',
                   'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
