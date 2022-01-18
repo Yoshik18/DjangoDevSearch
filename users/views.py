1fd643eb38a911ae1c83c60f768d40df3c0e4af5
Yoshik18 <yoshikkarnawat@gmail.com>
2022-01-18 19:32:09 +0530

    Done with styling registration, alert close not working

--- users/views.py
+++ users/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
+
 # Create your views here.
 
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -17,21 +23,44 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print('Username does not exist')
+            messages.error(request, 'Username does not exist')
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username or password is incorrect')
+            messages.error(request, 'Username or password is incorrect')
 
-    return render(request, 'users/login_register.html')
+    context = {'page': page}
+    return render(request, 'users/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(
+                request, 'An error has occurred during registration')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
